e0abe9dd6fe1464db4449492e5983ecf95b21121
Carlos Escobar <carlos.b.escobar@gmail.com>
2014-03-19 14:31:55 -0400

    changes from class

--- greensock-1/scripts/tween-1.js
+++ greensock-1/scripts/tween-1.js
@@ -21,20 +21,21 @@
 		// TweenMax.from($circle, 1, {scale: 0});
 		
 		// scale to nothing
-		TweenMax.fromTo($circle, 1, {scale: 0, top: 500}, {scale: 3, left: 1000, top: 0}); 
+		// TweenMax.fromTo($circle, 1, {scale: 0, top: 500}, {scale: 3, left: 1000, top: 0}); 
 		
 
 		// with easing
-		// TweenMax.to($circle, 1, {left: 1000, ease: 'Power4.easeOut'});
+		// TweenMax.to($circle, 1, {left: 1000, ease: 'Power1.easeOut'});
 		
 		// yoyo
-		// TweenMax.to($circle, 1, {left: 1000, ease: 'Power4.easeInOut', yoyo: true, repeat: 1});
+		// TweenMax.to($circle, 1, {left: 1000, ease: 'Power4.easeInOut', yoyo: true, repeat: 3});
 
 		// 2d transform
 		// TweenMax.to($circle, 1, {x: 1000});
 		
 		// 3d transform
-		// TweenMax.to($circle, 1, {x: 1000, z: 0.1});
+		// TweenMax.set('body', {})
+		TweenMax.to($circle, 1, {scale: 2, z: 0.1, ease: 'Power4.easeInOut', onComplete: function(){console.log('Hi Salem');}});
 		
 	});
 }(jQuery));
