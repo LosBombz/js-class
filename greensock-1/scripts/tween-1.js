363bb1fa74945dc4c806b67baa6e08bba659e0bc
Carlos Escobar <carlos.b.escobar@gmail.com>
2014-03-19 10:06:58 -0400

    first commit

---
+++ greensock-1/scripts/tween-1.js
@@ -0,0 +1,40 @@
+/*global jQuery:true, TweenMax:true*/
+(function($){
+	'use strict';
+
+
+
+	// doc ready
+	$(function(){
+		var $circle = $('.circle');	
+
+		// animate to the right
+		// TweenMax.to($circle, 1, {left: 1000});
+		
+		// scale to nothing
+		// TweenMax.to($circle, 1, {scale: 0});
+		
+		// from right
+		// TweenMax.from($circle, 1, {left: 1000});
+		
+		// scale to nothing
+		// TweenMax.from($circle, 1, {scale: 0});
+		
+		// scale to nothing
+		TweenMax.fromTo($circle, 1, {scale: 0, top: 500}, {scale: 3, left: 1000, top: 0}); 
+		
+
+		// with easing
+		// TweenMax.to($circle, 1, {left: 1000, ease: 'Power4.easeOut'});
+		
+		// yoyo
+		// TweenMax.to($circle, 1, {left: 1000, ease: 'Power4.easeInOut', yoyo: true, repeat: 1});
+
+		// 2d transform
+		// TweenMax.to($circle, 1, {x: 1000});
+		
+		// 3d transform
+		// TweenMax.to($circle, 1, {x: 1000, z: 0.1});
+		
+	});
+}(jQuery));
